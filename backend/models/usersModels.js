e45b7925cefc9fa9a5f3198e140a4088ffc4ab8d
200~Jeman-Kumar-97-06 <jeman060397@gmail.com>
2025-06-05 23:01:04 +0530

    made changes to user controllers

--- backend/models/usersModels.js
+++ backend/models/usersModels.js
@@ -10,27 +10,35 @@ const userSchema = new Schema ({
 });
 
 userSchema.statics.login = async function(){
+    //Check if all the fields are filled :
     if(!email || !password) {
         throw Error("All fields must be filled!")
     }
+    //See if you can find a user document with the same email : 
     const user = await this.findOne({email});
+    //If such a user doesn't exist throw an err saying the user with that email doesn't exist : 
     if (!user) {
         throw Error("Wrong username/ user with that name doesn't exist");
     }
+    //See if the password is right:
     const match = await bcrypt.compare(password,user.password);
     if (!match) {
         throw Error("Wrong password!")
     }
+    //Return the user object:
     return user;
 }
 
 userSchema.statics.signup = async function(){
+    //See if all the fields are filled:
     if(!fullname || !email || !password) {
         throw Error("All fields must be filled!")
     }
+    //Validate and see if the password is strong enough : 
     if(!validator.isStrongPassword(password)){
         throw Error("Password ain't strong enough!")
     }
+    
     const exists = await this.findOne({email});
     if (exists) {
         throw Error("User already exists")
