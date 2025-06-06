a0d581fdfc40d07c1f9c741a1c08679cbc5a4c9e
200~Jeman-Kumar-97-06 <jeman060397@gmail.com>
2025-06-06 22:17:11 +0530

    made changes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -37,7 +37,7 @@ export default function Navbar() {
         <input
           type="text"
           placeholder="Search wallpapers..."
-          className={`${theme === 'light' ? 'light' : 'dark'} dark:bg-gray-600 dark:text-white bg-white w-full border rounded-full px-4 py-2 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400`}
+          className={`${theme === 'light' ? 'light' : 'dark'} dark:bg-gray-700 dark:text-white bg-white w-full border rounded-full px-4 py-2 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400`}
         />
       </div>
 
