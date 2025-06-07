a020496cee10a18734d37c7ad52b81b278647134
200~Jeman-Kumar-97-06 <jeman060397@gmail.com>
2025-06-07 14:49:30 +0530

    made changes to userconstollers.js

--- backend/controllers/usersControllers.js
+++ backend/controllers/usersControllers.js
@@ -13,8 +13,47 @@ const client = new OAuth2Client(process.env.GOOGLECLIENTID);
 const googleLogin = async (req,res) => {
     const {credential} = req.body;
     try {
-        const ticket = await client.verifyIdToken
+        const ticket = await client.verifyIdToken({
+            idToken : credential,
+            audience : process.env.GOOGLECLIENTID
+        })
+        const payload = ticket.getPayload();
+        const {email,name,sub:googleId} = payload;
+        let user = await User.findOne({email});
+        if (!user) {
+            user = await User.create({
+                name,email,password:''
+            })
+        }
+        //Create a JWT:
+        const token = createToken(user._id);
+        return res.status(200).json({...user,token});
+    }
+    catch(error) {
+        console.log(error);
+        return res.status(400).json({message:"Google Login Failed!"})
     }
 }
 
+//Logging in with details filled manually:
+const loginUser = async (req,res) => {
+    const {email,password} = req.body;
+    try{
+        const user = await User.login(email,password);
+        const token = createToken(user._id);
+        return res.status(200).json({...user,token});
+    } catch(error) {
+        return res.status(404).json({error:error.message});
+    }
+};
 
+//Signup Controller:
+const signupUser = async (req,res) => {
+    const {fullname,email,password} = req.body;
+    try {
+        const user = await User.signup(fullname,email,password);
+        
+    } catch(error) {
+        return res.status(404).json({error:error.message})
+    }
+}
