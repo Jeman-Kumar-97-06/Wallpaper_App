f1eb36c3a4afb5d1b54895b51b6fbd06d3e6c80c
200~Jeman-Kumar-97-06 <jeman060397@gmail.com>
2025-06-18 11:13:57 +0530

    made changes

--- backend/middleware/requireAuth.js
+++ backend/middleware/requireAuth.js
@@ -10,7 +10,6 @@ const requireAuth = async (req,res,next) => {
     const token = authorization.split(' ')[1];
     try {
         const {_id} = jwt.verify(token,process.env.SEC);
-        console.log(_id)
         req.user    = await User.findOne({_id}).select('_id');
 
         next();
