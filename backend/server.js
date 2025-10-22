ae7365a9b9eb578479c958d3d6721d9229d553fd
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2025-10-22 19:39:51 +0530

    made changes

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,15 @@ app.use('/api/users',uRts);
 app.use('/api/walls',wRts);
 
 
+//When deploying : 
+if (process.env.NODE_ENV === "production") {
+    const clientPath = path.join(__dirname,'../client/build');
+    app.use(express.static(clientPath));
+    app.get('*',(req,res)=>{
+        res.sendFile(path.join(clientPath,'index.html'));
+    })
+}
+
 mongoose.connect(process.env.MONGOURL).then(()=>{
     app.listen(process.env.PORT,()=>{console.log("Connected to database and listening to request at ",process.env.PORT)})
 }).catch(error=>{console.log(error)});
