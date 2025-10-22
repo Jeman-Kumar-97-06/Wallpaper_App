277d842e779f40fa061d4bee96bf8986272761aa
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2025-10-22 19:53:59 +0530

    made changes;

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -64,7 +64,7 @@ export default function Navbar() {
                   handleEnterKey()
           }}}
           type="text"
-          placeholder="Type anything & press Enter"
+          placeholder="Type anything/nothing & press Enter"
           className={`${theme === 'light' ? 'light' : 'dark'} dark:bg-black dark:text-white bg-white w-full border rounded-full px-4 py-2 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400`}
         />
       </div>
